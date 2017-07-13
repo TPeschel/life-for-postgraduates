1af0c7cfa92d29d45338a0976f29ba020ab16769
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2017-07-13 18:57:57 +0200

    dft

--- Practise/percentiles.2017.07.13.R
+++ Practise/percentiles.2017.07.13.R
@@ -95,7 +95,7 @@ all.ages <-
 d <-
 	data.frame(
 		alter = sort( all.ages ),
-		motivation = ( 3 + ( 2 * cos( .5 +.00025 * c( 1 : n ) ) ** 2 ) * rnorm( n, 3, 2 ) ) + ( 4 + cos( .001 * c( 1 : n ) ) ) + ( .0001 * c( 1 : n - n / 2 ) ) ** 3 )
+		motivation = ( 1 + ( 2 * cos( .5 +.00025 * c( 1 : n ) ) ** 2 ) * rnorm( n, 3, 2 ) ) + ( 4 + cos( .001 * c( 1 : n ) ) ) + ( .0001 * c( 1 : n - n / 2 ) ) ** 3 )
 
 d$motivation <-
 	10 * log10( 1 + abs( d$motivation ) + d$motivation ) + d$motivation
@@ -121,6 +121,7 @@ d <-
 		ggplot( d ) +
 		geom_point( aes( alter, motivation ), alpha = .1 ) +
 		theme_bw( ) )
+
 ggsave( filename = paste0( "plot", nrow( d ), "orig.png" ), width = 16, height = 8 )
 # create.data.frame.for.percentile.curve.calculation.via.data.frame( 
 # 	data.frame = d, 
